8520696b42e08d26e75f0339aa97097891b2eb0f
unknown <josuedaniel831@yahoo.com>
2022-11-05 09:16:25 -0700

    Added twentynineteen the posts navigation pluggable function from the parent theme template-tags.php to child theme functions.php

--- wp-content/themes/linkedinlearningclassictheme/functions.php
+++ wp-content/themes/linkedinlearningclassictheme/functions.php
@@ -12,3 +12,24 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version') 
     );
 }
+
+/**
+ * Pluggable function from template-tags.php in the parent theme.
+ */
+function twentynineteen_the_posts_navigation() {
+    the_posts_pagination(
+        array(
+            'mid_size'  => 2,
+            'prev_text' => sprintf(
+                '%s <span class="nav-prev-text">%s</span>',
+                twentynineteen_get_icon_svg( 'chevron_left', 22 ),
+                __( 'Newer', 'twentynineteen' )
+            ),
+            'next_text' => sprintf(
+                '<span class="nav-next-text">%s</span> %s',
+                __( 'Older', 'twentynineteen' ),
+                twentynineteen_get_icon_svg( 'chevron_right', 22 )
+            ),
+        )
+    );
+}
